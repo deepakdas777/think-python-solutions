248e87165185504fbe4f3ac7d5ccfb53bb683baa
deepakdas777 <deepakdasvkd007@gmail.com>
2016-09-22 08:40:33 +0530

    first commit

---
+++ classes-and-methods/17.1.py
@@ -0,0 +1,19 @@
+# Rewrite time_to_int (from Section 16.4) as a method. It is probably not
+# appropriate to rewrite int_to_time as a method; it's not clear what object
+# you would invoke it on!
+
+
+
+
+class Time(object):
+    def time_to_int(self):
+        minutes = time.hour * 60 + time.minute
+        seconds = minutes * 60 + time.second
+        return seconds
+
+time = Time()
+time.hour = 11
+time.minute = 59
+time.second = 30
+
+print time.time_to_int()
